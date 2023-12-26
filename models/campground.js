6a9155ded7df64ab0e20e0871803069be9691a42
Snaehath <2020ad0556@svce.ac.in>
2023-12-26 20:45:59 +0530

    review model completed

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const Review = require('./review');
+const review = require('./review');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -12,8 +14,18 @@ const CampgroundSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref:'Review'
         }
-]
+    ]
 
 });
+
+CampgroundSchema.post('findOneAndDelete',async function (doc) {
+    if(doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews 
+            }
+        })
+    }
+})
 
 module.exports = mongoose.model('Campground',CampgroundSchema)
