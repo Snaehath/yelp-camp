10912734b0d755fb312e72ada38a4769f98e745c
Snaehath <2020ad0556@svce.ac.in>
2023-12-29 10:31:35 +0530

    added session and flash for actions

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const path = require('path')
-const ejsMate = require('ejs-mate')
-const ExpressError = require('./utils/ExpressError')
-const override = require('method-override')
-const campgrounds = require('./routes/campground')
-const reviews = require('./routes/review')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
+const ExpressError = require('./utils/ExpressError');
+const override = require('method-override');
+const campgrounds = require('./routes/campground');
+const reviews = require('./routes/review');
+const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
     .then(() =>{
@@ -16,17 +18,36 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
         console.log('Oh no MONGO error')
     })
 
-app.engine('ejs',ejsMate)
-app.set('view engine','ejs')
-app.set('views',path.join(__dirname,'views'))
+app.engine('ejs',ejsMate);
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
 
 
-app.use(express.urlencoded({extended:true}))
-app.use(override('_method'))
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.urlencoded({extended:true}));
+app.use(override('_method'));
+app.use(express.static(path.join(__dirname,'public')));
 
-app.use('/campgrounds',campgrounds)
-app.use('/campgrounds/:id/reviews',reviews)
+const sessionConfig ={
+    secret: 'iambatman',
+    resave: false,
+    saveUninitialized:true,
+    cookie:{
+        httpOnly: true,
+        expire: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req,res,next) =>{
+    res.locals.success = req.flash('success')
+    res.locals.error = req.flash('error')
+    next();
+})
+
+app.use('/campgrounds',campgrounds);
+app.use('/campgrounds/:id/reviews',reviews);
 
 app.get('/',(req,res) =>{
     res.send('WELCOME TO HOME PAGE!!')
