9f108472ad67c8abd972adfa5a5a4d3b973648ef
Snaehath <2020ad0556@svce.ac.in>
2024-01-01 22:04:28 +0530

    added cloud storage for image uploading

--- routes/campground.js
+++ routes/campground.js
@@ -3,10 +3,17 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync')
 const campgrounds = require('../controller/campground')
 const { isLoggedIn,validatecampground,isAuthor } = require('../middleware');
+const multer = require('multer')
+const {storage} = require('../cloudinary')
+const upload = multer({storage})
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validatecampground, catchAsync(campgrounds.createCampground))
+    // .post(isLoggedIn, validatecampground, catchAsync(campgrounds.createCampground))
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.body,req.files)
+        res.send("It worked!!")
+      })
 
 router.get('/new', isLoggedIn, campgrounds.renderForm)
 
